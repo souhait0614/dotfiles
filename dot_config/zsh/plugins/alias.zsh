f7d7b5f9d297fe9761d6575bb36109a291e51857
souhait <souhait.0614@gmail.com>
2025-05-04 09:15:21 +0900

    コマンドのエイリアスを設定

---
+++ dot_config/zsh/plugins/alias.zsh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+# alias
+alias ls="eza --git"
+alias la="eza -a --git"
+alias ll="eza -ahl --git"
+alias lt="eza -T -L 3 -a -I 'node_modules|.git|.cache'"
+alias lta="eza -T -a -I 'node_modules|.git|.cache' --color=always | less -r"
+alias l="clear && ls"
+alias code="code-insiders"
