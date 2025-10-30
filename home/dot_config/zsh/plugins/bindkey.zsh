c602083605a28e8a38da24c50a0e47faa2c982fd
souhait <me@souhait.me>
2025-10-30 12:06:19 +0900

    zsh: bindkeyプラグインを追加

---
+++ home/dot_config/zsh/plugins/bindkey.zsh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+
+# Key bindings
+bindkey "^A"          beginning-of-line
+bindkey "^E"          end-of-line
+bindkey "^[f"         forward-word
+bindkey "^[b"         backward-word
+bindkey "^[[1;3C"     forward-word
+bindkey "^[[1;3D"     backward-word
+bindkey "^[^?"        backward-kill-word
+bindkey "^[[killline" vi-kill-line
+bindkey "^I"          menu-select
+bindkey "^R"          fzf-select-history
+bindkey "^X"          fzf-cd-src
+bindkey "^F"          fzf-cdr
+
+bindkey               "$terminfo[kcbt]" menu-select
+bindkey -M menuselect              "^I" menu-complete
+bindkey -M menuselect "$terminfo[kcbt]" reverse-menu-complete
